16a404b94312af367967791d1ff145ab466aa344
Dwivedi Priya <pd7668665@gmail.com>
2024-12-17 13:21:51 +0530

    Nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Stack } from '@mui/material'
+import logo from './../assets/images/Logo.png'
 
 const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
+    return(
+        <>
+        <Stack direction='row' justifyContent='space-around' sx={{gap:{sm:'128px',xs:'40px'},mt:{sm:'32px',xs:'20px'},justifyContent:'none'}} px='20px'>
+            <Link to='/'>
+                <img src={logo} alt='logo' style={{width:'48px',height:'48px',margin:'0 20px'}}/>
+            </Link>
+            <Stack direction='row' gap='40px' fontSize='20px' alignItems='flex-end'>
+                <Link to='/'>Home</Link>
+                <a href='/exercise' style={{textDecoration:'none',color:'#3A1212',borderBottom:'3px #FF2625 solid'}}>Exercise</a>
+            </Stack>
+        </Stack>
+        </>
+    )
 }
 
 export default NavBar
