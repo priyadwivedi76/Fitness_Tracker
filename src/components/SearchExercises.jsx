09625055921efce1813e6441ca6962520e5a89cf
Dwivedi Priya <pd7668665@gmail.com>
2024-12-24 12:04:38 +0530

    Page

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import { useEffect,useState } from 'react'
+import { Stack,Typography,TextField,Button,Box } from '@mui/material'
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
+    <Stack alignItems='center' p='10vh' mt='5vh' justifyContent='center'>
+      <Typography fontWeight='700' fontSize={{lg:'3rem',sm:'2em',xs:'1em'}} mt='5vh' textAlign='center'>
+        Awesome Exercise you <br/> should know
+      </Typography>
+      <Box position='relative' mb='5vh'>
+        <TextField height='5vh' value='' onChange={(e)=>{}} placeholder='Search Exercises' type='text' sx={{input:{fontWeight:'600',borderRadius:'2vh',border:'none'},width:{lg:'80vw',xs:'60vw'}}} backgroundColor='#fff' borderRadius=''/>
+          <Button>
+            Search
+          </Button>
+      </Box>
+    </Stack>
   )
 }
 
